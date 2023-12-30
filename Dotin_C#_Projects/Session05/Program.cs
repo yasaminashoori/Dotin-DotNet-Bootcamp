43b59a9022c07dfe3404e1a3a00f8f3ff31b91d6
YasaminAshoori <yasaminashoori18@gmail.com>
2023-12-30 22:12:12 +0330

    📘add session5

---
+++ Dotin_C#_Projects/Session05/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Session05
+        }
+    }
+}
