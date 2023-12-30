60f3f9f2d5680c6f7d5e598862a2b3a5a3034cf4
YasaminAshoori <yasaminashoori18@gmail.com>
2023-12-30 22:10:24 +0330

    Add Common

---
+++ Dotin_C#_Projects/Common/Binary_Beasts_Tools/Extension/IEnumExt.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Binary_Beasts_Tools.Extension
+{
+    public static class IEnumExt
+    {
+        public static string Print(this IEnumerable<int> numbers)
+        {
+            var result = string.Empty;
+
+            foreach (var number in numbers)
+            {
+                result += $"{number},";
+            }
+
+            return result.RemoveLastCharacter();
+        }
+    }
+}
---
+++ Dotin_C#_Projects/Common/Binary_Beasts_Tools/Utility/NumericHelper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Binary_Beasts_Tools.Utility
+{
+    internal class NumericHelper
+    {
+        public double CalculatePercentage(long value)
+        {
+            if (value < 0 || value > number)
+            {
+                throw new ArgumentOutOfRangeException(nameof(value), "Value must be between 0 and the specified number.");
+            }
+
+            return (double)value / number * 100;
+        }
+
+        public void DisplayPercentage(long value)
+        {
+            try
+            {
+                double percentage = CalculatePercentage(value);
+                Console.WriteLine($"The percentage of {value} in {number} is: {percentage:F2}%");
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
