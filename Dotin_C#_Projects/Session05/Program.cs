816568b6f503d9ee35bed2ce335a863652c702d5
YasaminAshoori <yasaminashoori18@gmail.com>
2024-01-04 22:38:08 +0330

    add session5 person class prop

--- Dotin_C#_Projects/Session05/Program.cs
+++ Dotin_C#_Projects/Session05/Program.cs
@@ -30,4 +30,25 @@ namespace Session05
             return $"My name is {_name} and I'm {_age} years old.";
         }
     }
+
+    public class Person
+    {
+        private readonly string _name;
+        private readonly int _age;
+
+        public Person(string name, int age)
+        {
+            _name = name;
+            _age = age;
+        }
+
+        public string Introduce()
+        {
+            return $"My name is {_name} and I'm {_age} years old.";
+        }
+
+        public string Name { get { return _name; } }
+
+        public int Age { get { return _age; } }
+    }
 }
