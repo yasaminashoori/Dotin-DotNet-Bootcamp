21549dfed780a3fac301152dedbb5bfaa4b07dd8
yasaminashoori <yasaminashoori18@gmail.com>
2024-01-05 22:39:08 +0330

    Add MVCPart1 Controllers

---
+++ Dotin_C#_Projects/MVCExamples.Part01/Controllers/PersonController.cs
@@ -0,0 +1,34 @@
+﻿// PersonController.cs
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using MVCExamples.Part01.Models;
+
+namespace MVCExamples.Part01.Controllers
+{
+    public class PersonController : Controller
+    {
+        private List<Person> GetPersons()
+        {
+            return new List<Person>
+            {
+                new Person { Id = 1, FirstName = "John", LastName = "Doe" },
+                new Person { Id = 2, FirstName = "Jane", LastName = "Smith" },
+                new Person { Id = 3, FirstName = "Mary", LastName = "Doubra" },
+                new Person { Id = 4, FirstName = "Roz", LastName = "Ocanner" },
+
+
+            };
+        }
+
+        public IActionResult GetAll()
+        {
+            List<Person> persons = GetPersons();
+            return View(persons);
+        }
+    }
+}
+
+
+
+
