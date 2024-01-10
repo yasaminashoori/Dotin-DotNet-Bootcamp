c1fa4370aff18d70fdd2567a3332a95f0f792f40
yasaminashoori <yasaminashoori18@gmail.com>
2024-01-10 22:01:24 +0330

    📄 Add Part03

---
+++ CSProjects/CSProjects_Part03/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CSProjects_Part03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbersList = new int[] { 0, 1, 432, 32, 42, 5, 4, 7, 25, 92, 532, 354, 73342343, 7653, 76767, 4532, 125 };
+            var linq01 = from n in numbersList select n;
+            foreach (var n in numbersList)
+            {
+                Console.WriteLine(linq01);
+            }
+            Console.ReadKey();
+        }
+    }
+}
