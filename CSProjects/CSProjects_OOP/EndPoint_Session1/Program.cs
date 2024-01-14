9fde71ceb3d7a33f6677118513aedd78a58480ab
yasaminashoori <yasaminashoori18@gmail.com>
2024-01-14 23:32:43 +0330

    OOP Endpoint 1 and 2

---
+++ CSProjects/CSProjects_OOP/EndPoint_Session1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EndPoint_Session1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Models.Personel.Person obj1 = new Models.Personel.Person(); 
+            obj1.FirstName = "Test";
+            obj1.LastName = "Test";
+             
+        }
+    }
+}
---
+++ CSProjects/CSProjects_OOP/Models/Personel/Person.cs
@@ -0,0 +1,40 @@
+﻿namespace Models.Personel
+{
+    /// <summary>
+    /// کلاسی برای موجودیت اشخاص
+    /// </summary>
+    public class Person
+    {
+        private string _firstName;
+        private string _lastName;
+
+        public string FirstName
+        {
+            get
+            {
+                return _firstName;
+            }
+            set
+            {
+                _firstName = value;
+            }
+        }
+
+        public string LastName
+        {
+            get
+            {
+                return _lastName;
+            }
+            set
+            {
+                if (value.Length < 3)
+                    _lastName = "DEFAULT_NAME";
+                _lastName = value;
+            }
+        }
+
+        public bool Gender{ get; set;}
+
+    }
+}
