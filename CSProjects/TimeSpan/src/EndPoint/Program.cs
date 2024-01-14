20f3feeef67f64a4b10ce5fde0ec1bdffe3c297a
yasaminashoori <yasaminashoori18@gmail.com>
2024-01-14 23:23:39 +0330

    ⚡🔨 Complete TimeSpan OOP Task

---
+++ CSProjects/TimeSpan/src/EndPoint/Program.cs
@@ -0,0 +1,39 @@
+﻿using Models.User;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EndPoint
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var userOne = new User()
+            {
+                FirstName = "Maede",
+                LastName = "Zohrabi",
+                BirthDate = new DateTime(2001, 5, 15)
+            };
+
+            var userTwo = new User()
+            {
+                FirstName = "Yasamin",
+                LastName = "Ashoori",
+                BirthDate = new DateTime(2001, 4, 10)
+            };
+
+            Console.ForegroundColor = ConsoleColor.Magenta; 
+
+            Console.WriteLine(userOne);
+            Console.WriteLine(userTwo);
+            TimeSpan ageDifference = userOne.UserCompareTime(userTwo);
+            Console.WriteLine($"{ageDifference.Days} Days.");
+
+            Console.ReadKey();  
+        }
+    }
+}
---
+++ CSProjects/TimeSpan/src/Entity/Models/User/User.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.User
+{
+    /// <summary>
+    /// کلاسی برای گرفتن اطلاعات شخصی کاربر
+    /// </summary>
+    public class User
+    {
+        public User()
+        {
+        }
+
+        public User(string FirstName, string LastName, DateTime BirthDate)
+        {
+            _firstName = FirstName;
+            _lastName = LastName;
+            _birthDate = BirthDate;
+        }
+
+        private string _firstName;
+        private string _lastName;
+        private DateTime _birthDate;
+
+        public string FirstName
+        {
+            get { return _firstName; }
+            set { _firstName = value; }
+        }
+
+        public string LastName
+        {
+            get { return _lastName; }
+            set { _lastName = value; }
+        }
+
+        public DateTime BirthDate
+        {
+            get { return _birthDate; }
+            set { _birthDate = value; }
+        }
+
+        public TimeSpan UserCompareTime(User otherUser)
+        {
+            TimeSpan timeDifference = this._birthDate - otherUser._birthDate;
+            Console.Write("The difference of the user BirthDate is: ");
+            return timeDifference;
+        }
+
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName}";
+        }
+    }
+}
