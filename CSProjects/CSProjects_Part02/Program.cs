e0416603b2445aa8736efcc4e350358454f89c4d
yasaminashoori <yasaminashoori18@gmail.com>
2024-01-14 23:33:47 +0330

    Add Part2 Split Examples

--- CSProjects/CSProjects_Part02/Program.cs
+++ CSProjects/CSProjects_Part02/Program.cs
@@ -10,7 +10,28 @@ namespace CSProjects_Part02
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Split with multiple separators:");
+            // Split with multiple separators
+            string multiCharString = "Mahesh..Chand, Henry\n He\t, Chris-Love, Raj..Beniwal, Praveen-Kumar";
+            // Split authors separated by a comma followed by space
+            string[] multiArray = multiCharString.Split(new Char[] { ' ', ',', '.', '-', '\n', '\t' });
+            foreach (string author in multiArray)
+            {
+                if (author.Trim() != "")
+                    Console.WriteLine(author);
+            }
 
+            Console.WriteLine("Split String delimited by another string: ");
+
+            string stringString = "Mahesh Chand, Neel Chand Beniwal, Raj Beniwal, Dinesh Beniwal";
+            // String separator
+            string[] stringSeparators = new string[] { "Beniwal, ", "Chand, " };
+            string[] firstNames = stringString.Split(stringSeparators, StringSplitOptions.None);
+            foreach (string firstName in firstNames)
+                Console.WriteLine(firstName);
+
+
+            Console.ReadKey(); 
         }
     }
 }
--- CSProjects/Common/YasTools/Utility/StringHelper.cs
+++ CSProjects/Common/YasTools/Utility/StringHelper.cs
@@ -9,12 +9,29 @@ namespace YasTools.Utility
     /// <summary>
     /// /// برای متدهای سودمند رشته‌ها
     /// </summary>
-    public class StringHelper
+    public static class StringHelper
     {
         public static bool IsNUllOrEmptyZero(string str)
         {
             return (string.IsNullOrEmpty(str)) || (str.Equals("0")  || (str.Length == 0));
         }
+
+        public static string ToPascalCase(string str)
+        {
+            // soroUSH ==> Soroush
+
+            if (IsNUllOrEmptyZero(str))
+                return str;
+
+            str = str.ToLower();
+
+            // return +
+
+            return string.Concat(str.Substring(0, 1).ToUpper(), str.Substring(1));
+
+        }
+
+
     }
 
 }
