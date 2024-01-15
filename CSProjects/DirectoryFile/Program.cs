b5797d0bb7f740bd8bc817f2cddd9cd0593d658b
yasaminashoori <yasaminashoori18@gmail.com>
2024-01-15 16:09:12 +0330

    ⚡🔨 Finish Directory File Task

--- CSProjects/DirectoryFile/Program.cs
+++ CSProjects/DirectoryFile/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -12,20 +14,73 @@ namespace DirectoryFiles
     {
         static void Main(string[] args)
         {
-            DirectoryInfo infoPlace = new DirectoryInfo("C:\\Program Files\\Windows Defender");
+            DirectoryInfo infoPlace = new DirectoryInfo("C:\\Users\\Yasi\\Desktop\\all");
 
             FileInfo[] filesList = infoPlace.GetFiles();
 
             Console.ForegroundColor = ConsoleColor.Green;
+
             foreach (FileInfo i in filesList)
             {
                 Console.WriteLine("File Name: {0}\n" +
-                    "File Length: {1}\n" +
+                    "File Length: {1} KB \n" +
                     "File Extension: {2}\n",
                     i.Name,
-                    i.Length,
+                    i.Length / 1024,
                     i.Extension);
                 System.Threading.Thread.Sleep(1000);
+            }
+
+            var sortedFiles = from file in filesList orderby file.Length select file);
+
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+
+            foreach (FileInfo file in sortedFiles)
+            {
+                Console.WriteLine($"{file.Name.Substring(0, 6)} Size:{file.Length / 1024} KB ");
+                System.Threading.Thread.Sleep(1000);
+            }
+
+
+            var countedNames = filesList
+                          .Where(file => file.Extension == ".sql")
+                          .Select(file => file.Name);
+
+            var countedNumbers = countedNames.Count();
+
+            Console.ForegroundColor = ConsoleColor.Magenta;
+
+            Console.WriteLine($"The count of files: {countedNumbers}");
+            foreach (var item in countedNames)
+            {
+                Console.WriteLine($"The counted items are: {item}");
+            }
+
+            #region Solution1_Count
+            //int countExtension = countedFiles.Count();
+            //var countExtension = filesList.Count(n => n.Extension.ToLower() == ".sql".ToLower());
+            //Console.WriteLine($"Number of files with extension {".sql"}: {countExtension}");
+            //foreach (var fileName in countedFiles)
+            //{
+            //    Console.WriteLine(fileName);
+            //} 
+            #endregion
+
+
+            #region Solution1_Hidden
+            //DirectoryInfo directory = new DirectoryInfo(@"C:\Users\Yasi\Desktop\gitFile");
+            //FileInfo[] HiddenFiles = directory.GetFiles();
+
+            //var filtered = HiddenFiles.Where(f => !f.Attributes.HasFlag(FileAttributes.Hidden)); 
+            #endregion
+
+            var files = new DirectoryInfo(@"C:\Users\Yasi\Desktop\gitFile")
+                            .GetFiles().Where(x => (x.Attributes & FileAttributes.Hidden) == 0);
+
+
+            foreach (var f in files)
+            {
+                Console.WriteLine(f.FullName);
             }
 
             Console.ReadKey();
