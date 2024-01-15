c8118c5ed35f3709bc661a3b5f543b4a434fe245
yasaminashoori <yasaminashoori18@gmail.com>
2024-01-15 16:08:41 +0330

    ⚡🔨 Finishe Guess Number MVC 🎲

---
+++ CSProjects/MVCExamples/MVCExamples_GuessNumber/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVCExamples_GuessNumber.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
