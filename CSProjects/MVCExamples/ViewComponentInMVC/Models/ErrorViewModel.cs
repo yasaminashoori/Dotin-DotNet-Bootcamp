24533df4b0c6fe944fa38d3aa487bad8f188d4f0
yasaminashoori <yasaminashoori18@gmail.com>
2024-01-25 00:22:06 +0330

    🛠️ View Components for Products table MVC

---
+++ CSProjects/MVCExamples/ViewComponentInMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ViewComponentInMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ CSProjects/MVCExamples/ViewComponentInMVC/ViewComponents/TopProductsViewComponent.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ViewComponentInMVC.Models;
+namespace ViewComponentInMVC.ViewComponents
+{
+    public class TopProductsViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync(int count)
+        {
+            // This is my logic for preparing data
+            ProductRepository productRepository = new ProductRepository();
+            var books = await productRepository.GetTopProductsAsync(count);
+            return View(books);
+        }
+    }
+}
