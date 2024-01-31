4770192563c26e30c2de9f111bcf6ae443bc0e8e
yasaminashoori <yasaminashoori18@gmail.com>
2024-01-31 23:01:39 +0330

    🛠️ MVC: Add New MVC Folder for previous projects

---
+++ MVC_Projects/Domain/Entities/Person.cs
@@ -0,0 +1,72 @@
+﻿namespace Entities;
+
+/// <summary>
+/// موجودیت اشخاص
+/// </summary>
+public class Person(int id, string firstName, string lastName)
+{
+    /// <summary>
+    /// شناسه شخص
+    /// </summary>
+    public int ID { get; set; } = id;
+   
+    /// <summary>
+    /// نام شخص
+    /// </summary>
+    public string FirstName { get; private set; } = firstName;
+   
+    /// <summary>
+    /// نام خانوادگی
+    /// </summary>
+    public string LastName { get; private set; } = lastName;
+
+    /// <summary>
+    /// توضیحات
+    /// </summary>
+    public string? Note { get; set; }
+   
+    /// <summary>
+    /// آیا مدیر است ؟
+    /// </summary>
+    public bool IsAdmin { get; set; }
+  
+    public override string ToString()
+    {
+        return $"{ID} {FirstName} {LastName}";
+    }
+}
+
+
+
+
+public class Product
+{
+
+    //public Product()
+    //{
+
+    //}
+
+    public Product(int id, string title, int price)
+    {
+        ID = id;
+        Title = title;
+        Price = price;
+    }
+
+    public Product(int id, string title)
+    {
+        ID = id;
+        Title = title;
+        Price = 0;
+    }
+
+    public int ID { get; set; } 
+    public string Title { get; private set; } 
+    public int Price { get; private set; }
+
+    public override string ToString()
+    {
+        return $"{ID} {Title} {Price}";
+    }
+}
---
+++ MVC_Projects/HTML_Helper/Controllers/PersonController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace HTML_Helper.Controllers;
+
+public class PersonController : Controller
+{
+    public IActionResult Index()
+    {
+        var people = SampleData.Personel.GetPeople();
+        return View(people);
+    }
+}
+
