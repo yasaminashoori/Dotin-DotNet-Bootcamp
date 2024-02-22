3d03b2a16e269003ff543a4f01863c41f3551cc8
yasaminashoori <yasaminashoori18@gmail.com>
2024-02-22 23:58:27 +0330

    add attributes

---
+++ NH_Examples/Attributes/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Attributes.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ NH_Examples/Attributes/Models/MyAttribute.cs
@@ -0,0 +1,20 @@
+﻿
+namespace Attributes.Models
+{
+    [AttributeUsage(AttributeTargets.Class, Inherited = false)]
+    public class ClassAttribute : Attribute
+    {
+        public string Name { get; set; }
+        public int Number { get; set; }
+    }
+    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Constructor, AllowMultiple = true)]
+    public class MethodOrConstructor : Attribute
+    {
+        public string Name { get; set; }
+    }
+    [AttributeUsage(AttributeTargets.All)]
+    public class MyAttribute : Attribute
+    {
+
+    }
+}
