4e51e8e8586e7af5510863bb58c6aba20772161c
yasaminashoori <yasaminashoori18@gmail.com>
2024-04-02 22:36:35 +0330

    Add WF Calculator (review)

---
+++ Dotin_C#_Projects/Session04/WFCalculator/WFCalculator/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WFCalculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
--- Dotin_C#_Projects/Session04/WFCalculator/WFCalculator/Properties/AssemblyInfo.cs
+++ Dotin_C#_Projects/Session04/WFCalculator/WFCalculator/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Session04")]
+[assembly: AssemblyTitle("WFCalculator")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Session04")]
-[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyProduct("WFCalculator")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("9c2dd4e3-1369-4e93-82cc-803033b5010d")]
+[assembly: Guid("98a2cd9e-5006-44a1-b97e-2df72c41fe0d")]
 
 // Version information for an assembly consists of the following four values:
 //
