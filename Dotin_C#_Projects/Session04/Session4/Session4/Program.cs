93d5c0fa2799ef14b098ff61f58bdece6a65ed04
yasaminashoori <yasaminashoori18@gmail.com>
2024-04-03 23:35:06 +0330

    add enumaration

---
+++ Dotin_C#_Projects/Session04/Session4/Session4/Program.cs
@@ -0,0 +1,29 @@
+﻿
+
+namespace Session4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "Hello";
+
+            // Because string implements IEnumerable, we can call GetEnumerator():
+            IEnumerator rator = s.GetEnumerator();
+
+            while (rator.MoveNext())
+            {
+                char c = (char)rator.Current;
+                Console.Write(c + ".");
+            }
+
+            Console.WriteLine();
+
+            // Equivalent to:
+
+            foreach (char c in s)
+                Console.Write(c + ".");
+
+        }
+    }
+}
